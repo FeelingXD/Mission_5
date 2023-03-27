561aed03e75a75b40267c0f1393e5f59ba941f20
FeelingXD <wlals425315@gmail.com>
2023-03-28 04:57:09 +0900

    Chore: refresh status

---
+++ user-api/src/main/java/com/zerobase/cms/user/client/MailgunClient.java
@@ -0,0 +1,17 @@
+package com.zerobase.cms.user.client;
+
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
+import feign.Response;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name="mailgun" ,url="https://api.mailgun.net/v3/")
+@Qualifier
+public interface MailgunClient {
+
+    @PostMapping("${mailgun.api.domain}"+"/messages")
+    Response sendEMail(@SpringQueryMap SendMailForm form);
+}
