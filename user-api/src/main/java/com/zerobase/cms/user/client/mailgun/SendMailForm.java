33fe1c124d62f6067cddce734f8cd920ea08c5dc
FeelingXD <wlals425315@gmail.com>
2023-03-27 20:12:46 +0900

    feat : [001-EMAIL-Connect] mailgun에 대한 email 연동
    무료계정으로 테스트 시 이메일 등록이 필요합니다.

---
+++ user-api/src/main/java/com/zerobase/cms/user/client/mailgun/SendMailForm.java
@@ -0,0 +1,18 @@
+package com.zerobase.cms.user.client.mailgun;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class SendMailForm {
+    String from;
+    String to;
+    String subject;
+    String text;
+
+}
---
+++ user-api/src/main/java/com/zerobase/cms/user/service/EmailSendService.java
@@ -0,0 +1,23 @@
+package com.zerobase.cms.user.service;
+
+import com.zerobase.cms.user.client.MailgunClient;
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
+import feign.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSendService {
+    private final MailgunClient mailgunClient;
+
+    public Response sendEmail(){
+        SendMailForm form= SendMailForm.builder()
+                .from("testAccount@no-reply.com")
+                .to("wlals425315@gmail.com")
+                .subject("test subject")
+                .text("test text")
+                .build();
+        return mailgunClient.sendEMail(form);
+    }
+}
