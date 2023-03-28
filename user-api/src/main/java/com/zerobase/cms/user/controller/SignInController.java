4155a0b087ec371fbea20c1b4a66748994dbae7a
FeelingXD <wlals425315@gmail.com>
2023-03-29 06:02:08 +0900

    fix : fix scratch result

--- user-api/src/main/java/com/zerobase/cms/user/controller/SignInController.java
+++ user-api/src/main/java/com/zerobase/cms/user/controller/SignInController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/signIn")
+@RequestMapping("/signin")
 public class SignInController {
 
     private final SignInApplication signInApplication;
