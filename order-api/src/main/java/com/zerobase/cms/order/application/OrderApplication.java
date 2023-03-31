63bc1576b49b46da279161a158bc9dbcd4b6a5ca
FeelingXD <wlals425315@gmail.com>
2023-03-31 13:59:56 +0900

    feat: [17-ORDER-EMAIL] 상품주문 이메일발송

--- order-api/src/main/java/com/zerobase/cms/order/application/OrderApplication.java
+++ order-api/src/main/java/com/zerobase/cms/order/application/OrderApplication.java
@@ -8,7 +8,10 @@ import com.zerobase.cms.order.domain.redis.Cart;
 import com.zerobase.cms.order.exception.CustomException;
 import com.zerobase.cms.order.exception.ErrorCode;
 import com.zerobase.cms.order.service.ProductItemService;
+import com.zerobase.cms.user.config.client.MailgunClient;
+import com.zerobase.cms.user.config.client.mailgun.SendMailForm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,14 +19,18 @@ import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderApplication {
 
     private final CartApplication cartApplication;
     private final UserClient userClient;
     private final ProductItemService productItemService;
+    private final MailgunClient mailgunClient;
 
     @Transactional
     public void order(String token, Cart cart) {
+
+        StringBuilder mailText=new StringBuilder("");
         // 1번 : 주문 시 기존 카트 버림.
         // 2번 : 선택주문 : 내가 사지 않은 아이템을 살려야 함
         // -- 여러분이 숙제
@@ -49,9 +56,25 @@ public class OrderApplication {
             for(Cart.ProductItem cartItem : product.getItems()){
                 ProductItem productItem = productItemService.getProductItem(cartItem.getId());
                 productItem.setCount(productItem.getCount()-cartItem.getCount());
+
+                //메일 내용 추가
+                mailText.append("\n상품 : " +productItem.getProduct().getName()+ " 옵션 : " + productItem.getName() +" 가격 : " +productItem.getPrice());
             }
         }
 
+        //주문내역 메일보내기
+
+        mailText.append("\n 총가격 : "+ totalPrice);
+        log.info("mailText",mailText);
+
+        SendMailForm sendMailForm = SendMailForm.builder()
+                .from("zeroOrder@no-reply.com")
+                .to(customerDto.getEmail())
+                .subject("주문이 완료되었습니다.")
+                .text(mailText.toString())
+                .build();
+
+        mailgunClient.sendEMail(sendMailForm);
     }
 
 
